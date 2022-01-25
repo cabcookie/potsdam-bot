db46740a19bbd48000bb4ff7dcbd0c88642f5006
Carsten Koch <carsten.b.koch@gmail.com>
2022-01-25 14:52:55 +0100

    feat: change cron job to 30 sec

--- lib/lambda-crawler-stack.ts
+++ lib/lambda-crawler-stack.ts
@@ -1,34 +1,34 @@
 import { Duration, Stack, StackProps } from "aws-cdk-lib";
 import { Rule, Schedule } from "aws-cdk-lib/aws-events";
 import { LambdaFunction } from "aws-cdk-lib/aws-events-targets";
-import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
-// import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
 
 export class LambdaCrawlerStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const lambda = new Function(this, 'LambdaTest', {
-      code: Code.fromAsset('functions/crawl-page'),
-      handler: 'index.handler',
-      runtime: Runtime.NODEJS_14_X,
-    });
-
-    // const lambdaCrawler = new NodejsFunction(this, 'CrawlPotsdamPage', {
-    //   functionName: 'crawlPotsdamPage',
+    // const lambda = new Function(this, 'LambdaTest', {
+    //   code: Code.fromAsset('functions/crawl-page'),
+    //   handler: 'index.handler',
     //   runtime: Runtime.NODEJS_14_X,
-    //   handler: 'handler',
-    //   entry: './functions/crawl-page/index.ts',
-    //   // timeout: Duration.minutes(5),
-    //   // memorySize: 2048,
-    //   // environment: { REGION: this.region },
     // });
 
-    const rule = new Rule(this, 'CronJobToRunCrawler', {
-      schedule: Schedule.rate(Duration.minutes(5)),
+    const lambdaCrawler = new NodejsFunction(this, 'CrawlPotsdamPage', {
+      functionName: 'crawlPotsdamPage',
+      runtime: Runtime.NODEJS_14_X,
+      handler: 'handler',
+      entry: './functions/crawl-page/index.ts',
+      // timeout: Duration.minutes(5),
+      // memorySize: 2048,
+      // environment: { REGION: this.region },
     });
 
-    rule.addTarget(new LambdaFunction(lambda));
+    const rule = new Rule(this, 'CronJobToRunCrawler', {
+      schedule: Schedule.rate(Duration.seconds(30)),
+    });
+
+    rule.addTarget(new LambdaFunction(lambdaCrawler));
   }
 }
