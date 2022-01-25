2d889b9eb0f93405d16823d025e0db176a861927
Carsten Koch <carsten.b.koch@gmail.com>
2022-01-26 00:24:39 +0100

    feat: Lambda writes a file on an S3 bucket

--- functions/crawl-page/index.ts
+++ functions/crawl-page/index.ts
@@ -1,16 +1,33 @@
-import axios from 'axios';
-import cheerio from 'cheerio';
+import { S3 } from 'aws-sdk';
 
 const potsdamUrl = 'https://egov.potsdam.de/tnv/?START_OFFICE=buergerservice';
+const s3Upload = (fileName: string, content: string) => {
+  const bucket = new S3();
+  bucket.upload({
+    Bucket: process.env.BUCKETNAME || 'test-bucket-87654',
+    Key: fileName,
+    Body: content,
+  }, (err, data) => {
+    if (err) throw err;
+    console.log(`File uploaded successfully: ${data.Location}`);
+  });
+};
 
 exports.handler = async () => {
-  const result = await axios(potsdamUrl);
-  const html = cheerio.load(result.data);
+  if (!process.env.BUCKETNAME) {
+    return {
+      statusCode: 400,
+      headers: { "Content-Type": "text/json" },
+      body: JSON.stringify({
+        error: 'ERROR: BUCKETNAME is not defined as an environment variable',
+      }),
+    };
+  };
+
+  s3Upload('helloworld.json', 'Hello World!');
 
   const logMessage = {
     message: "Hello from Lambda",
-    result: result.data,
-    html,
   };
   console.log("Result", JSON.stringify(logMessage));
 
--- lib/lambda-crawler-stack.ts
+++ lib/lambda-crawler-stack.ts
@@ -3,11 +3,25 @@ import { Rule, Schedule } from "aws-cdk-lib/aws-events";
 import { LambdaFunction } from "aws-cdk-lib/aws-events-targets";
 import { Runtime } from "aws-cdk-lib/aws-lambda";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Bucket, BucketEncryption, HttpMethods } from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
 
 export class LambdaCrawlerStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    const bucket = new Bucket(this, 'ScreenshotsPotsdamBot', {
+      bucketName: 'screenshots-potsdam-bot',
+      encryption: BucketEncryption.S3_MANAGED,
+      cors: [{
+        allowedOrigins: ['*'],
+        allowedMethods: [
+          HttpMethods.POST,
+          HttpMethods.PUT,
+        ],
+        allowedHeaders: ['*'],
+      }],
+    });
 
     const lambdaCrawler = new NodejsFunction(this, 'CrawlPotsdamPage', {
       functionName: 'crawlPotsdamPage',
@@ -16,9 +30,12 @@ export class LambdaCrawlerStack extends Stack {
       timeout: Duration.minutes(1),
       // memorySize: 2048,
       entry: './functions/crawl-page/index.ts',
-      // bundling: {
-      //   externalModules: ['aws-sdk'],
-      // },
+      bundling: {
+        externalModules: ['aws-sdk'],
+      },
+      environment: {
+        BUCKETNAME: bucket.bucketName,
+      }
       // environment: { REGION: this.region },
     });
 
@@ -27,5 +44,7 @@ export class LambdaCrawlerStack extends Stack {
     });
 
     rule.addTarget(new LambdaFunction(lambdaCrawler));
+
+    bucket.grantWrite(lambdaCrawler);
   }
 }
