fe924f10869c7d986e9e96534a73608f9bc5a25f
Carsten Koch <carsten.b.koch@gmail.com>
2022-01-25 15:52:56 +0100

    feat: fetching the potsdam page with axios/cheerio

--- functions/crawl-page/index.ts
+++ functions/crawl-page/index.ts
@@ -1,6 +1,16 @@
+import axios from 'axios';
+import cheerio from 'cheerio';
+
+const potsdamUrl = 'https://egov.potsdam.de/tnv/?START_OFFICE=buergerservice';
+
 exports.handler = async () => {
+  const result = await axios(potsdamUrl);
+  const html = cheerio.load(result.data);
+
   const logMessage = {
     message: "Hello from Lambda",
+    result: result.data,
+    html,
   };
   console.log("Result", JSON.stringify(logMessage));
 
