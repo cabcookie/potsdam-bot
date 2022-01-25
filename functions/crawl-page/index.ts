986a23ee1f03cb728f0d63fd502d39823c9ecf26
Carsten Koch <carsten.b.koch@gmail.com>
2022-01-25 15:19:27 +0100

    fix: allow for build without docker

--- functions/crawl-page/index.ts
+++ functions/crawl-page/index.ts
@@ -1,9 +1,12 @@
 exports.handler = async () => {
+  const logMessage = {
+    message: "Hello from Lambda",
+  };
+  console.log("Result", JSON.stringify(logMessage));
+
   return {
     statusCode: 200,
     headers: { "Content-Type": "text/json" },
-    body: JSON.stringify({
-      message: "Hello from Lambda",
-    })
+    body: JSON.stringify(logMessage),
   }
 }
--- lib/lambda-crawler-stack.ts
+++ lib/lambda-crawler-stack.ts
@@ -9,24 +9,21 @@ export class LambdaCrawlerStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // const lambda = new Function(this, 'LambdaTest', {
-    //   code: Code.fromAsset('functions/crawl-page'),
-    //   handler: 'index.handler',
-    //   runtime: Runtime.NODEJS_14_X,
-    // });
-
     const lambdaCrawler = new NodejsFunction(this, 'CrawlPotsdamPage', {
       functionName: 'crawlPotsdamPage',
       runtime: Runtime.NODEJS_14_X,
       handler: 'handler',
-      entry: './functions/crawl-page/index.ts',
       // timeout: Duration.minutes(5),
       // memorySize: 2048,
+      entry: './functions/crawl-page/index.ts',
+      // bundling: {
+      //   externalModules: ['aws-sdk'],
+      // },
       // environment: { REGION: this.region },
     });
 
     const rule = new Rule(this, 'CronJobToRunCrawler', {
-      schedule: Schedule.rate(Duration.seconds(30)),
+      schedule: Schedule.rate(Duration.minutes(1)),
     });
 
     rule.addTarget(new LambdaFunction(lambdaCrawler));
