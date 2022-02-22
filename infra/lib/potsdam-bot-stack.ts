0e8eb3e71ed3cd9504dd0a44f861e32bf617a6a7
Carsten Koch <carsten.b.koch@gmail.com>
2022-02-22 12:55:32 +0100

    fix(unstable): adding just the pipeline should remove everything else

--- infra/lib/potsdam-bot-stack.ts
+++ infra/lib/potsdam-bot-stack.ts
@@ -2,7 +2,7 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
 import { githubRepo } from '../../config';
-import { PBotCrawlerStage } from './pbot-crawler-stage';
+// import { PBotCrawlerStage } from './pbot-crawler-stage';
 
 export class PotsdamBotStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -20,9 +20,9 @@ export class PotsdamBotStack extends Stack {
       }),
     });
 
-    pipeline.addStage(new PBotCrawlerStage(this, 'PBotCrawlerStage', {
-      stackName: 'PBotCrawlerStage',
-      description: 'Creating the Lambda function which crawls the desired URL.',
-    }));
+    // pipeline.addStage(new PBotCrawlerStage(this, 'PBotCrawlerStage', {
+    //   stackName: 'PBotCrawlerStage',
+    //   description: 'Creating the Lambda function which crawls the desired URL.',
+    // }));
   }
 }
